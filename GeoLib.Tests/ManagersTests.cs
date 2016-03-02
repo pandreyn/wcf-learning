4de4f14a9e1dde2ba7f892a4706f0f883ac28f05
Andrey Petkevich <pandreyn@gmail.com>
2016-03-02 20:24:33 +0300

    begin adding tests

---
+++ GeoLib.Tests/ManagersTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using GeoLib.Data;
+
+namespace GeoLib.Tests
+{
+    [TestClass]
+    public class ManagerTests
+    {
+        [TestMethod]
+        public void test_zip_code_retrieval()
+        {
+            Mock<IZipCodeRepository> mockZipCodeRepository = new Mock<IZipCodeRepository>();
+        }
+    }
+}
